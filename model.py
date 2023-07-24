a07444721d43123b8007a388df62ee3ca29a300e
Jonathan Whitaker <johnowhitaker@gmail.com>
2023-07-24 12:22:52 -0700

    Add config options for flash and quiet models

--- model.py
+++ model.py
@@ -130,6 +130,8 @@ class GPTConfig:
     n_embd: int = 768
     dropout: float = 0.0
     bias: bool = True # True: bias in Linears and LayerNorms, like GPT-2. False: a bit better and faster
+    flash: bool = False # Whether to use flash attention if it is available
+    quiet: bool = False # Whether to use QuietAttention
 
 class GPT(nn.Module):
 
